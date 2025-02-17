1a431654a8645615bdd0255b28c07b27cb374cd8
xtofuub <99559348+xtofuub@users.noreply.github.com>
2025-02-17 20:53:07 +0200

    Update email.php
    Signed-off-by: xtofuub <99559348+xtofuub@users.noreply.github.com>

--- email.php
+++ email.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Email Spoofer</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="icon" type="image/svg+xml" href="favicon.svg">
     <style>
                 * {
             margin: 0;
